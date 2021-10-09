75db7e3fa6d7282023d610a82fceacc6b533e857
Aadit Rahul Kamat <aadit.k12@gmail.com>
2021-10-09 10:28:24 +0800

    Add tagui cask

---
+++ tagui.rb
@@ -0,0 +1,9 @@
+cask "tagui" do
+  version "6.46.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/kelaberetiv/TagUI/releases/download/v#{version}/TagUI_macOS.zip",
+      verified: "github.com/kelaberetiv/TagUI/"
+  name "TagUI"
+  desc "Free Robot Process Automation (RPA) tool developed by AI Singapore"
+  homepage "https://github.com/kelaberetiv/TagUI"
+end
